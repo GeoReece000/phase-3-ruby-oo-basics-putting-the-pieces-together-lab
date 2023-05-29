fb846f809260542c9e4573e4d15f19f1afa24632
GeoReece000 <geofreytegeret@gmail.com>
2023-05-29 20:38:13 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,8 @@
+class Book
+  attr_accessor :title, :author_name, :page_count, :genre
 
+  def initialize title, author_name, page_count, genre
+    @title= title
+end
 
+title= Book.new ("And Then There Were None")
